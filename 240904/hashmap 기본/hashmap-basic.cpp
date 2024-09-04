79a383881efed2dff3aba59bc4979888aa7e804a
KuJuHeon <155406526+KuJuHeon@users.noreply.github.com>
2024-09-04 15:32:30 +0900

    Title: hashmap 기본, Time: 115ms, Memory: 0MB, Status: Passed - CodeTree

--- 240904/hashmap 기본/hashmap-basic.cpp
+++ 240904/hashmap 기본/hashmap-basic.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    unordered_map<int,int> m;
+    for(int i=0;i<n;++i){
+        string ord;
+        cin>>ord;
+        if(ord=="add"){
+            int a,b;
+            cin>>a>>b;
+            m[a]=b;
+        }
+        else if(ord=="remove"){
+            int a;
+            cin>>a;
+            m.erase(a);
+        }
+        else if(ord=="find"){
+            int a;
+            cin>>a;
+            if(m.find(a) == m.end()) cout<<"None\n";
+            else cout<<m[a]<<"\n";
+        }
+    }
     return 0;
 }
