d7c64d168d0dbee5fe26212347acf08a420a435a
KuJuHeon <155406526+KuJuHeon@users.noreply.github.com>
2024-09-11 21:46:40 +0900

    Title: 자주 등장한 top k 숫자, Time: 42ms, Memory: 3MB, Status: Passed - CodeTree

--- 240911/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
+++ 240911/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-bool compare(pair<int,int> &a, pair<int,int> &b){
+bool compare(pair<int,int> a, pair<int,int> b){
     if(a.second == b.second) return a.first>b.first;
     return a.second>b.second;
 }
