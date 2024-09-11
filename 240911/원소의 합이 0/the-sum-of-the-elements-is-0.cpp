3f408c61f8b54cc3f5f90b0227c7e71a7fb2477d
KuJuHeon <155406526+KuJuHeon@users.noreply.github.com>
2024-09-11 21:58:11 +0900

    Title: 원소의 합이 0, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240911/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
+++ 240911/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -11,6 +11,8 @@ int A[MAX_N], B[MAX_N], C[MAX_N], D[MAX_N];
 unordered_map<int, int> freq;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     // 입력:
     cin >> n;
     for(int i = 0; i < n; i++) cin >> A[i];
