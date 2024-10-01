511328cfd43bfdd3f746e0b5ebb103c707c53194
KuJuHeon <155406526+KuJuHeon@users.noreply.github.com>
2024-10-01 17:43:50 +0900

    Title: 컨베이어 벨트, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241001/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int matrix[2][200], candi[2][200];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, t;
+    cin >> n >> t;
+    for (int y = 0; y < 2; ++y) {
+        for (int x = 0; x < n; ++x) {
+            cin >> matrix[y][x];
+        }
+    }
+    int move_x = t % n;
+    for (int y = 0; y < 2; ++y) {
+        for (int x = 0; x < n; ++x) {
+            int move_y = (x + t) / n;
+            int ny = (y + move_y + 2) % 2;
+            int nx = (x + move_x + n) % n;
+            candi[ny][nx] = matrix[y][x];
+        }
+    }
+
+    for (int y = 0; y < 2; ++y) {
+        for (int x = 0; x < n; ++x) {
+            cout << candi[y][x] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
